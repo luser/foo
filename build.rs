645e20a323b7f53d95880fc7ad15cdfd67c9031c
Ted Mielczarek <ted@mielczarek.org>
2019-02-20 11:25:51 -0500

    make build reproducible with MSVC

--- build.rs
+++ build.rs
@@ -1,7 +1,12 @@
 extern crate cc;
 
 fn main() {
-    cc::Build::new()
+    let mut build = cc::Build::new();
+    if build.get_compiler().is_like_msvc() {
+        // MSVC outputs timestamps in object files without this flag.
+        build.flag("/Brepro");
+    }
+    build
         .file("foo.c")
         .compile("foo");
 }
